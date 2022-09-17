06f139915b0f0965bc88e0c46535f6c9a3463237
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2022-09-17 01:41:17 -0500

    feat (puppeteer) add manual chatting

--- pages/api/join_chat.js
+++ pages/api/join_chat.js
@@ -1,21 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import tmi from "tmi.js";
 
-const client = new tmi.Client({
-  options: { debug: true },
-  connection: {
-    reconnect: true,
-    secure: true,
-  },
-  identity: {
-    username: "tumatador666",
-    password: "oauth:72zzrqtavoqnpmlhr33wf1tozgn4cy",
-  },
-  channels: ["forsen"],
-});
-
-client.connect();
 export default async function handler(req, res) {
+  const { token } = req.query;
+  const client = new tmi.Client({
+    options: { debug: true },
+    connection: {
+      reconnect: true,
+      secure: true,
+    },
+    identity: {
+      username: "tumatador666",
+      password: "oauth:"+token,
+    },
+    channels: ["forsen"],
+  });
+
+  client.connect();
   client.on("message", (channel, tags, message, self) => {
     console.log(channel, self);
     // Ignore echoed messages.
@@ -24,15 +25,12 @@ export default async function handler(req, res) {
     }
   });
 
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 1000; i++) {
     (async () => {
       setTimeout(async () => {
-        await client.say(
-          "forsen",
-          `, veryFors ${String.fromCharCode([
-            0x2800,
-          ]).repeat(i)}`
-        );
+        let left = i % 2 === 0 ? String.fromCharCode([0x2800]) : "";
+        let right = i % 2 !== 0 ? String.fromCharCode([0x2800]) : "";
+        await client.say("forsen", `${left} 7tvM ${right}`);
       }, i * 1000);
     })();
   }
--- pages/api/manual_chat.js
+++ pages/api/manual_chat.js
@@ -1,14 +1,32 @@
 import puppeteer from "puppeteer";
 
-export default function handler(req, res) {
-  (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("https://www.twitch.tv/forsen");
-    await page.screenshot({ path: "example.png" });
+export default async function handler(req, res) {
+  const authToken = "s7dckj4u6xcf6ur3uswyeumxejmwwz";
+  const browser = await puppeteer.launch();
 
-    await browser.close();
-  })();
+  const cookies = [
+    {
+      name: "auth-token",
+      value: authToken,
+    },
+  ];
+
+  const page = await browser.newPage();
+  await page.goto("https://twitch.tv");
+  await page.waitForSelector('[data-a-target*="login-button"]');
+  const button = await page.$('[data-a-target*="login-button"]');
+  await button.click();
+  await page.evaluate(() => {
+    localStorage.setItem("mature", "true");
+    localStorage.setItem("video-muted", '{"default":false}');
+    localStorage.setItem("volume", "0.5");
+    localStorage.setItem("video-quality", '{"default":"160p30"}');
+  });
+
+  await page.setViewport({ width: 1280, height: 720 });
+  await page.setCookie(...cookies);
+
+  await page.screenshot({ path: "example.png" });
 
   res.status(200).json({ name: "sent message" });
 }
